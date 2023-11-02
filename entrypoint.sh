b295e4339131a0da97ed9dbfaba4f1fb43e9595f
Dirk Jamieson <i.got.burnt@gmail.com>
2023-11-03 09:31:22 +1000

    tail log file

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,5 +12,10 @@ export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 export STEAM_COMPAT_CLIENT_INSTALL_PATH="$STEAMDIR"
 export STEAM_COMPAT_DATA_PATH="$STEAMDIR/steamapps/compatdata/2430930"
 
+LOG_FILE=/game/ShooterGame/Saved/Logs/ShooterGame.log
+mkdir -p `basename $LOG_FILE`
+touch $LOG_FILE
+tail -f /game/ShooterGame/Saved/Logs/ShooterGame.log -n 0
+
 cd "/game/ShooterGame/Binaries/Win64"
 exec $STEAMDIR/compatibilitytools.d/$PROTON_NAME/proton run ArkAscendedServer.exe $LAUNCH_ARGS
